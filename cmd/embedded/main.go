5678735492d164ef49f4c2209b8b1370abeaac06
marcpires <marcpiresrj@gmail.com>
2025-07-08 11:56:43 -0300

    feature: Add examples of struct and misuse of type embedding

---
+++ cmd/embedded/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/marcpires/rss/pkg/inmem"
+)
+
+// Foo represents something.
+type Foo struct {
+	Bar
+}
+
+// Bar represents something.
+// Bar field is promoted to Foo
+type Bar struct {
+	Baz int
+}
+
+// embedMisuse demonstrates a misuse of type embedding
+// as Lock and UnLock methods gets promoted and is visible to
+// external clients.
+func embedMisuse() {
+	m := inmem.New()
+	m.Lock()
+}
+
+func main() {
+	foo := Foo{}
+	foo.Baz = 23
+
+	fmt.Printf("foo.Baz value is %d is promoted", foo.Baz)
+	fmt.Printf("foo.Bar.Baz value is %d", foo.Bar.Baz)
+
+	// embedMisuse()
+}
---
+++ cmd/langtypes/main.go
@@ -0,0 +1,46 @@
+package main
+
+import "fmt"
+
+// user represents a system user type.
+type user struct {
+	name  string
+	email string
+	ext   int
+	admin bool
+}
+
+type admin struct {
+	person user
+	level  string
+}
+
+func main() {
+
+	// Declare a user using struct literal.
+	// Field order does not matter.
+	marcp := user{
+		admin: true,
+		email: "marcpires@gmail.com",
+		name:  "Marc Pires",
+		ext:   123,
+	}
+
+	// Declaring a user using just the values.
+	// Order matters
+	bruno := user{"bruno@lhc.net.br", "Bruno", 223, false}
+
+	douglas := admin{
+		person: user{
+			name:  "Douglas",
+			email: "douglas@lhc.net",
+			ext:   334,
+			admin: true,
+		},
+		level: "super",
+	}
+
+	fmt.Println(marcp)
+	fmt.Println(bruno)
+	fmt.Println(douglas)
+}
