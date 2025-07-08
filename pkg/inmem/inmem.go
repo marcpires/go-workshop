4f8b5e68940f643b2e1c00f6a3d7cff04be2e77e
marcpires <marcpiresrj@gmail.com>
2025-07-08 11:57:23 -0300

    feature: Add examples of struct and misuse of type embedding

---
+++ pkg/inmem/inmem.go
@@ -0,0 +1,19 @@
+package inmem
+
+import "sync"
+
+type MemData struct {
+	sync.Mutex
+	memo map[string]int
+}
+
+func New() *MemData {
+	return &MemData{memo: make(map[string]int)}
+}
+
+func (i *MemData) Get(k string) (int, bool) {
+	i.Lock() // Acessing the Lock directly
+	v, contains := i.memo[k]
+	i.Unlock()
+	return v, contains
+}
